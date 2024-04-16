e8b3db1fb27c955e22a3c4f40d60aac37c51ddcb
guilbert <guilbertmartin75@gmail.com>
2024-04-17 00:57:47 +0200

    cart.html done

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.querySelector("#button").addEventListener("click", function (){
     // console.log("found: ", found)
         document.querySelector("#thirddivhome").innerHTML = `<div></div>`
         for (let i = 0; i < data.length; i++){
-            document.querySelector("#thirddivhome").innerHTML += `<div class ="row">
+            document.querySelector("#thirddivhome").innerHTML += `<div class ="row" data-testid = ${data[i]._id}>
                 <div data-departure = ${data[i].departure}>${data[i].departure}</div>
                 <div>></div>
                 <div data-arrival = ${data[i].arrival}>${data[i].arrival}</div>
@@ -27,27 +27,22 @@ document.querySelector("#button").addEventListener("click", function (){
             </div>`
         }
     })
+    .then(() => {
+        const addToCartArray = document.querySelectorAll(".addToCart");
+        for (let cart = 0; cart < addToCartArray.length; cart ++){
+            addToCartArray[cart].addEventListener("click", function (){ //addEventListener to add dans cart mongo-DB
+                fetch("http/localhost:3000/", { 
+                    method: "POST",
+                    headers: { "Content-Type": "applicatiion/json" },
+                    body: JSON.tostringify({
+                        _id: this.data-test,
+                    })
+                })
+                window.location.href = "./cart.html";
+            })
+        }
+    })
 });
 
 
 
-//--------------------------addEventListener to add dans cart mongo-DB--
-
-    const addToCartArray = document.querySelectorAll(".addToCart");
-    for (let cart = 0; cart < addToCartArray.length; cart ++){
-        addToCartArray[cart].addEventListener("click", function (){
-            fetch("http/localhost:3000/", {
-                method: "POST",
-                headers: { "Content-Type": "applicatiion/json" },
-                body: JSON.tostringify({
-                    departure: this.firstChild.data-departure,
-                    arrival: ,
-                    date: ,
-                    price: ,
-                })
-            })
-            .then (() => {
-                window.location.href = "";
-            })
-        })
-    }
