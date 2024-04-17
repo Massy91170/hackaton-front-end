3a4b2a8f94f06e0eb184f08094a6f4ec26049c4e
guilbert <guilbertmartin75@gmail.com>
2024-04-17 11:29:41 +0200

    display ok, wait for backend push

--- scriptCart.js
+++ scriptCart.js
@@ -2,6 +2,7 @@ fetch("http/localhost:3000/cart")
 .then (response => response.json())
 .populate("trajet")
 .then (data => {
+    console.log(data)
     //document.querySelector("#databaseContainer").innerHTML = `<div></div>` 
     for (let i = 0; i < data.length; i++){
         const pattern = /(\d{2}:\d{2})/;
@@ -13,7 +14,7 @@ fetch("http/localhost:3000/cart")
             </div>
             <div data-date = ${data[i].date}>${data[i].trajet.date.match(pattern)[0]}</div>
             <div data-price = ${data[i].price}>${data[i].trajet.price} €</div>
-            <button class="addToCart">add cart</button>
+            <button class="delete">X</button>
         </div>`
     }
 })
